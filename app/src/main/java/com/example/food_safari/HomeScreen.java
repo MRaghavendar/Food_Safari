92d828f637f2618d5e22ee580aaa320162a84a33
pradeepkumar <pradeepkumartheegala@gmail.com>
2020-02-10 11:47:57 -0600

    Added all buttons functionality and tested working

--- app/src/main/java/com/example/food_safari/HomeScreen.java
+++ app/src/main/java/com/example/food_safari/HomeScreen.java
@@ -4,15 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
+
+    Button accountBTN;
+    Button restaurantsBTN;
+    Button cartBTN;
+    Button ordersBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        Intent intent = new Intent();
+        accountBTN = findViewById(R.id.accountBTN);
+        restaurantsBTN = findViewById(R.id.restaurantsBTN);
+        cartBTN = findViewById(R.id.cartBTN);
+        ordersBTN = findViewById(R.id.ordersBTN);
+
+        accountBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeScreen.this,Account.class));
+            }
+        });
+
+        cartBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeScreen.this,Cart.class));
+            }
+        });
+
+        restaurantsBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeScreen.this,Home.class));
+            }
+        });
 
     }
 }
