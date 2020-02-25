795210c2df9c58f56cdd946d97b304087a2bdcec
pradeepkumar <pradeepkumartheegala@gmail.com>
2020-02-25 13:59:27 -0600

    Adding functionalities to delete account from database

--- app/src/main/java/com/example/food_safari/HomeScreen.java
+++ app/src/main/java/com/example/food_safari/HomeScreen.java
@@ -1,11 +1,21 @@
 package com.example.food_safari;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -13,6 +23,7 @@ public class HomeScreen extends AppCompatActivity {
     Button restaurantsBTN;
     Button cartBTN;
     Button ordersBTN, aboutBTN;
+    Button deleteAccBTN;
 
 
     @Override
@@ -24,30 +35,69 @@ public class HomeScreen extends AppCompatActivity {
         restaurantsBTN = findViewById(R.id.restaurantsBTN);
         cartBTN = findViewById(R.id.cartBTN);
         ordersBTN = findViewById(R.id.ordersBTN);
-        aboutBTN=findViewById(R.id.aboutBTN);
+        aboutBTN = findViewById(R.id.aboutBTN);
+        deleteAccBTN = findViewById(R.id.deleteAccBTN);
 
 
         accountBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(HomeScreen.this,Account.class));
+                startActivity(new Intent(HomeScreen.this, Account.class));
             }
         });
 
         aboutBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(HomeScreen.this,AboutUs.class));
+                startActivity(new Intent(HomeScreen.this, AboutUs.class));
             }
         });
-
-
 
 
         restaurantsBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(HomeScreen.this,Home.class));
+                startActivity(new Intent(HomeScreen.this, Home.class));
+            }
+        });
+
+        deleteAccBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(HomeScreen.this);
+                dialog.setTitle("Are you sure?");
+                dialog.setMessage("Deleting this account will completely removes your account and you won't be able to access this app");
+                dialog.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                        user.delete()
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(HomeScreen.this, "Deleted Successfully", Toast.LENGTH_SHORT).show();
+                                            Log.d("deleted", "User account deleted.");
+                                            Intent intent = new Intent(HomeScreen.this, LoginActivity.class);
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                            startActivity(intent);
+                                        } else {
+                                            Toast.makeText(HomeScreen.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                    }
+                });
+                dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog alertDialog = dialog.create();
+                alertDialog.show();
             }
         });
 
