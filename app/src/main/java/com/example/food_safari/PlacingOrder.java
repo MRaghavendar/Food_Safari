d032ce0b5fbc8eb8cbff29670806b7b25a883614
Rahulmallampati825 <rahulmallampati825@gmail.com>
2020-03-01 13:29:51 -0600

    Adding the references in Java from layout file

--- app/src/main/java/com/example/food_safari/PlacingOrder.java
+++ app/src/main/java/com/example/food_safari/PlacingOrder.java
@@ -1,14 +1,29 @@
 package com.example.food_safari;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class PlacingOrder extends AppCompatActivity {
+
+    EditText placeNameET;
+    EditText placeAddress1ET;
+    EditText placeAddress2ET;
+    EditText placePhoneET;
+
+    Button confirmBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_placing_order);
+        placeNameET = findViewById(R.id.placeNameET);
+        placeAddress1ET = findViewById(R.id.placeAddress1ET);
+        placeAddress2ET = findViewById(R.id.placeAddress2ET);
+        placePhoneET = findViewById(R.id.placePhoneET);
+
+        confirmBTN = findViewById(R.id.confirmBTN);
     }
 }
--- app/src/test/java/com/example/food_safari/ExampleUnitTest.java
+++ app/src/test/java/com/example/food_safari/ExampleUnitTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see <placeNameET href="http://d.android.com/tools/testing">Testing documentation</placeNameET>
  */
 public class ExampleUnitTest {
     @Test
