140b8372ceddba621f8ce997c470d6b4b6a74074
saikirandd <33051183+saikirandd@users.noreply.github.com>
2020-03-01 18:18:29 -0600

    changed cart

--- app/src/main/java/com/example/food_safari/Cart.java
+++ app/src/main/java/com/example/food_safari/Cart.java
@@ -1,4 +1,49 @@
 package com.example.food_safari;
 
-public class Cart {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Cart extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private Button NextProcessBtn;
+    private TextView txtTotalAmount;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+        recyclerView=findViewById(R.id.cart_list);
+        recyclerView.setHasFixedSize(true);
+        layoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        NextProcessBtn=(Button) findViewById(R.id.next_process_btn);
+        txtTotalAmount=(TextView) findViewById(R.id.total_price);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        final DatabaseReference cartListRef= FirebaseDatabase.getInstance().getReference().child("Cart List");
+
+
+//       FirebaseRecyclerOptions<CartModel> options=new FirebaseRecyclerOptions.Builder<CartModel>().setQuery(cartListRef.child("UserView")
+//               .child("FoodItems"), CartModel.class).build();
+
+
+    }
 }
