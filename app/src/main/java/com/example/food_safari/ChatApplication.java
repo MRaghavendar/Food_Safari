c706cb90ca6413cd336c04e07c57a88be6a27363
Nishanthreddy1997 <46684557+Nishanthreddy1997@users.noreply.github.com>
2020-03-03 14:08:41 -0600

    oncreate

--- app/src/main/java/com/example/food_safari/ChatApplication.java
+++ app/src/main/java/com/example/food_safari/ChatApplication.java
@@ -3,4 +3,8 @@ package com.example.food_safari;
 import android.app.Application;
 
 public class ChatApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
