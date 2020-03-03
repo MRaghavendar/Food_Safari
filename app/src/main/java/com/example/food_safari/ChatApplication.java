c6db75a2f63ec8fa06a1d4b7df5e1c39a1b5443f
Nishanthreddy1997 <46684557+Nishanthreddy1997@users.noreply.github.com>
2020-03-03 14:12:10 -0600

    commit

--- app/src/main/java/com/example/food_safari/ChatApplication.java
+++ app/src/main/java/com/example/food_safari/ChatApplication.java
@@ -6,5 +6,18 @@ public class ChatApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Settings settings = new Settings("YOUR_APP_ID");
+        settings.setMapsApiKey("YOUR_MAPS_API_KEY");
+
+        Smooch.init(this, settings, new SmoochCallback() {
+            @Override
+            public void run(Response response) {
+                if (response.getError() == null) {
+                    // Your code after init is complete
+                } else {
+                    // Something went wrong during initialization
+                }
+            }
+        });
     }
 }
