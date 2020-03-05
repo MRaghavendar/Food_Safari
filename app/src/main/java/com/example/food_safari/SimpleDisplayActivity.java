87fbd853fb0880ea3ca7c8f5e177b9115c70db60
Rahulmallampati825 <rahulmallampati825@gmail.com>
2020-03-05 15:26:40 -0600

    Changes made to display

--- app/src/main/java/com/example/food_safari/SimpleDisplayActivity.java
+++ app/src/main/java/com/example/food_safari/SimpleDisplayActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SimpleDisplayActivity extends AppCompatActivity {
 
     TextView textView_id,textView_name,textView_age;
     String tempUserName,tempUserId,tempUserAge;
+    Button pd_add_to_cart_button;
 
 
     @Override
@@ -22,10 +25,21 @@ public class SimpleDisplayActivity extends AppCompatActivity {
         tempUserName = intent.getStringExtra("userName");
         tempUserAge = intent.getStringExtra("userAge");
 
+
         //view init
         textView_id = findViewById(R.id.main_text_id);
         textView_name = findViewById(R.id.main_text_name);
         textView_age = findViewById(R.id.main_text_age);
+        pd_add_to_cart_button=findViewById(R.id.pd_add_to_cart_button);
+        pd_add_to_cart_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(new Intent(SimpleDisplayActivity.this,Cart.class));
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
 
         if(!tempUserAge.isEmpty() && !tempUserId.isEmpty() && !tempUserName.isEmpty())
         {
