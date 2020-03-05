87cb15987dee1081e47dc339d4f8f783e3ec2e00
Raghavendar Reddy Maddelavedu <raghavendarm10@gmail.com>
2020-03-05 00:12:39 -0600

    Developed the java class for xml page of home

--- app/src/main/java/com/example/food_safari/Home.java
+++ app/src/main/java/com/example/food_safari/Home.java
@@ -1,40 +1,64 @@
 package com.example.food_safari;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class Home extends AppCompatActivity {
-    Button medBTN, mongBTN, indBTN, MexiBTN, splOfBTN, splDish, trackBTN, backBTN;
-    Intent b1, b2;
+
+    private DatabaseReference ProductsRef;
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerOptions<Products> options = new FirebaseRecyclerOptions.Builder<Products>()
+                .setQuery(ProductsRef, Products.class).build();
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter = new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ProductViewHolder holder, int i, @NonNull Products model) {
+                holder.txtProductName.setText(model.getPname());
+                holder.txtProductDescription.setText(model.getDescription());
+                holder.txtProductPrice.setText("Timings " + model.getPrice());
+                Picasso.get().load(model.getImage()).into(holder.imageView);
+            }
+
+            @NonNull
+            @Override
+            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout, parent, false);
+                ProductViewHolder holder = new ProductViewHolder(view);
+                return holder;
+            }
+        };
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        medBTN = findViewById(R.id.medbtn);
-        medBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                b1 = new Intent(Home.this, Restaurant.class);
-                startActivity(b1);
 
+        ProductsRef = FirebaseDatabase.getInstance().getReference().child("Restaurants");
+        recyclerView = findViewById(R.id.restRecycler);
+        recyclerView.setHasFixedSize(true);
 
-
-            }
-        });
-        backBTN = findViewById(R.id.backBTN);
-        backBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                b2 = new Intent(Home.this, LoginActivity.class);
-                startActivity(b2);
-            }
-        });
-
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
     }
 
