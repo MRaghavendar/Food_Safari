50bde8892ba941b83367ed16629e4ce6ae56450d
Raghavendar Reddy Maddelavedu <raghavendarm10@gmail.com>
2020-03-05 13:09:31 -0600

    Product page and java class is updated

--- app/src/main/java/com/example/food_safari/ProductViewHolder.java
+++ app/src/main/java/com/example/food_safari/ProductViewHolder.java
@@ -4,33 +4,40 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ProductViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+public class ProductViewHolder extends RecyclerView.ViewHolder {
+
     public TextView txtProductName, txtProductDescription, txtProductPrice;
     public ImageView imageView;
-    public ItemClickListner listner;
+    public RecyclerView category_recyclerView;
+    public RecyclerView.LayoutManager manager;
 
 
     public ProductViewHolder(View itemView) {
         super(itemView);
 
+        manager = new LinearLayoutManager(itemView.getContext(), LinearLayoutManager.HORIZONTAL, false);
 
         imageView = itemView.findViewById(R.id.product_image);
         txtProductName = itemView.findViewById(R.id.Restaurant_Name);
         txtProductDescription = itemView.findViewById(R.id.Restaurant_Timings);
         txtProductPrice = itemView.findViewById(R.id.Restaurant_desc);
 
-    }
-
-    public void setItemClickListner(ItemClickListner listner) {
-        this.listner = listner;
+        category_recyclerView = itemView.findViewById(R.id.menuRecycler);
+        category_recyclerView.setLayoutManager(manager);
 
     }
 
-    @Override
-    public void onClick(View view) {
-        listner.onClick(view, getAdapterPosition(), false);
+//    public void setItemClickListner(ItemClickListner listner) {
+//        this.listner = listner;
+//
+//    }
 
-    }
+//    @Override
+//    public void onClick(View view) {
+//        listner.onClick(view, getAdapterPosition(), false);
+//
+//    }
 }
