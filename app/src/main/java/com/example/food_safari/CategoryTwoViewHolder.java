642ce49d190ffc7e15a6b0f98500db325cd79b0a
Raghavendar Reddy Maddelavedu <raghavendarm10@gmail.com>
2020-03-05 13:10:54 -0600

    Category and viewholder is created for recycler for menu

---
+++ app/src/main/java/com/example/food_safari/CategoryTwoViewHolder.java
@@ -0,0 +1,32 @@
+package com.example.food_safari;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CategoryTwoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView dataName;
+    public TextView dataId;
+    public TextView dataAge;
+    public ItemClickListner subCategoryOnClickInterface;
+
+    public CategoryTwoViewHolder(@NonNull final View itemView) {
+        super(itemView);
+        dataId = itemView.findViewById(R.id.data_id);
+        dataAge = itemView.findViewById(R.id.data_age);
+        dataName = itemView.findViewById(R.id.data_name);
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        subCategoryOnClickInterface.onClick(v, false);
+    }
+
+    public void SubCategoryInterfaceClick(ItemClickListner subCategoryOnClickInterface) {
+        this.subCategoryOnClickInterface = subCategoryOnClickInterface;
+    }
+}
